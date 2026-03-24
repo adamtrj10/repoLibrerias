ad14771d6a46a28e9c417c9f68ce0e929f1bf002
Adam <adamtrjk10@gmail.com>
2026-03-24 16:44:20 +0100

    Añadidos comentarios

--- libreriarecursos/lib/src/recursosWidgets/notificador.dart
+++ libreriarecursos/lib/src/recursosWidgets/notificador.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/foundation.dart';
 
+/// Gestiona un estado reactivo,
+/// implementando 'ValueListenable' para que los widgets puedan escuchar sus cambios.
+/// **Parámetros:** recibe un valor inicial genérico [T]
+/// **Devuelve** una instancia [Notificador] que notifica a sus oyentes
 class Notificador<T> extends ChangeNotifier implements ValueListenable<T> {
   T _value;
   Notificador(this._value);
 
+  /// Actualiza el valor interno y dispara la notificación a todos los widgets que escuchan
+  /// [nuevoValor] -> el nuevo dato que queremos almacenar
+  /// si es el mismo valor, no notificamos
   @override
   T get value => _value;
 
@@ -13,6 +20,11 @@ class Notificador<T> extends ChangeNotifier implements ValueListenable<T> {
   }
 }
 
+/// Extensión para cualquier tipo de objeto en Dart para crear un [Notificador]
+/// **Parámetros:** se aplica sobre cualquier instancia de tipo [T]
+/// **Lo que devuelve:** objeto de tipo [Notificador<T>]
 extension NotificadorExtension<T> on T {
+  /// getter que convierte una variable normal en una reactiva
+  ///  **Devuelve** una instancia de [Notificador] lista para ser usada con ValueListenableBuilder.
   Notificador<T> get ntf => Notificador<T>(this);
 }
